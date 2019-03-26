03a29941934b2dbfde9fa394ffe4b351457b22b6
Tony Pitale <tpitale@gmail.com>
2019-03-26 10:46:42 -0500

    Use sleep/wakeup triggers on thread to handle flushing; add specs

--- lib/staccato/adapter/batch.rb
+++ lib/staccato/adapter/batch.rb
@@ -6,54 +6,70 @@ module Staccato
       BatchTooBigError = Class.new(StandardError)
 
       DEFAULT_MAX_SIZE = 20
+      FLUSHING_TIMEOUT = 10
 
-      def initialize(adapter=Staccato.default_adapter, size=DEFAULT_MAX_SIZE)
-        raise BatchTooBigError.new("Batch size #{size} is larger than Google accepts.") if size > DEFAULT_MAX_SIZE
+      attr_reader :adapter
+
+      def initialize(adapter=Staccato.default_adapter, size: DEFAULT_MAX_SIZE, flush_timeout: FLUSHING_TIMEOUT)
+        if size > DEFAULT_MAX_SIZE
+          raise BatchTooBigError, "Batch size #{size} is larger than Google accepts."
+        end
 
         @size = size
         @adapter = adapter.new(Staccato.ga_batch_uri)
 
         @queue = SizedQueue.new(@size)
-        @flushing_thread = Thread.new { loop { flush } }
+        @flushing_thread = Thread.new { loop { sleep(flush_timeout); flush } }
       end
 
       def post(params)
         # will block if the queue is full
         # while the flushing thread will continue to dequeue
+
         @queue << params
+
+        # Try to flush when we get to max queue size here
+        if @queue.length >= @queue.max
+          # Trigger flushing thread
+          @flushing_thread.wakeup
+        end
       end
 
-      def clear
-        # kills thread, flushes anything left in local array
-        @flushing_thread.kill
-        @flushing_thread.join # wait for killing to complete
+      def clear(final: false)
+        if final
+          # kills thread, flushes anything left in local array
+          @flushing_thread.wakeup
+          @flushing_thread.kill
+          @flushing_thread.join # wait for killing to complete
+        end
 
-        flush(true) # flush anything left in the queue
+        flush # flush anything left in the queue
       end
 
       private
       # In a new thread collect params to a local array
       # when that array is at the batch size, POST to GA
       # then return and loop in the thread
-      def flush(final=false)
+      def flush
         param_array = []
 
         while param_array.size < @size
           # thread-safe, sized queue
-          # will block in this thread if nothing
-          # is available to dequeue, unless final is true
-          # then it is non_blocking, which raises a ThreadError
-          param_array << @queue.deq(final)
+          # avoid blocking, use non_blocking dequeue
+          # this raises a ThreadError when it runs
+          # out of elements in the queue
+          param_array << @queue.deq(true)
         end
       rescue ThreadError
         # the queue is empty, breaks out of the while loop
-        # only used for final flush
-      ensure # this may catch a thread dying?
-        # we may have to do something to format each as a new line when flushing
-        @adapter.post(batch_format(param_array))
+      ensure
+        unless param_array.empty?
+          # we may have to do something to format each as a new line when flushing
+          @adapter.post(batch_format(param_array))
+        end
       end
 
-      # convert our array of params hashes into a newline separated 
+      # convert our array of params hashes into a newline separated
       # form-encoded string for posting to GA's batch endpoint
       def batch_format(param_array=[])
         param_array.map {|params| URI.encode_www_form(params)}.join("\n")
